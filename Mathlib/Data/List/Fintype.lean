331cb2ccfe280b8760b8ce0d97ab8f565bad1708
Wrenna Robson <wrenna.robson@bristol.ac.uk>
2026-02-10 09:02:20 +0000

    Updates

--- Mathlib/Data/List/Fintype.lean
+++ Mathlib/Data/List/Fintype.lean
@@ -16,79 +16,139 @@ public import Mathlib.Data.FinEnum
 
 namespace List
 
-variable {α : Type*} {l : List α}
+universe u
 
-instance {v : α} : NeZero (v :: l).length := ⟨Nat.succ_ne_zero _⟩
+variable {α : Type u}
 
-attribute [coe] get
+instance : IsEmpty (Fin ([] : List α).length) := Fin.isEmpty
 
-/-- There is a coercion from `l : Type` to `α`. -/
-instance instCoeOutFinLength : CoeOut l α := ⟨l.get⟩
+instance {v : α} : Unique (Fin [v].length) := Fin.instUnique
 
-@[simp] theorem coe_mem {x : l} : ↑x ∈ l := List.get_mem _ _
+def ToType (l : List α) := {xi : α × Fin l.length // xi.1 = l[xi.2]}
+
+instance instCoeType {α : Type u} : CoeSort (List α) (Type u) := ⟨ToType⟩
+
+notation:max "↥" l:40  => ToType l
+
+namespace ToType
+
+variable {l l' : List α}
+
+def val (x : l) : α := x.1.1
+def idx (x : l) : Fin l.length := x.1.2
+
+@[simp, grind =] theorem val_eq {x : l} : x.val = l[x.idx] := x.2
+@[simp, grind =] theorem getElem_idx {x : l} : l[x.idx] =x.val := x.2.symm
+
+theorem get_comp_idx : l.get ∘ idx = val := funext fun _ => getElem_idx
+
+def mk (i : Fin l.length) : l := ⟨⟨l[i], i⟩, rfl⟩
+
+@[simp, grind =] theorem idx_mk {i : Fin l.length} : (mk i).idx = i := rfl
+@[simp, grind =] theorem val_mk {i : Fin l.length} : (mk i).val = l[i.1] := rfl
+
+@[simp, grind =] theorem mk_idx {x : l} : mk x.idx = x :=
+  Subtype.ext <| Prod.ext getElem_idx rfl
+
+@[cases_eliminator, elab_as_elim]
+def recOn {motive : l → Sort*} (mk : ∀ i : Fin l.length, motive (mk i)) (x : l) : motive x :=
+  x.mk_idx ▸ mk x.idx
 
 @[simp, grind =]
-theorem coe_mk {i : Nat} {hi : i < l.length} : ↑(Fin.mk i hi) = l[i] := rfl
+theorem recOn_mk {motive : l → Sort*} {mk : ∀ i : Fin l.length, motive (mk i)} (i : Fin l.length) :
+    recOn mk (ToType.mk i) = mk i := rfl
+
+@[ext] theorem ext {x y : l} : x.idx = y.idx → x = y := x.recOn <| y.recOn <| by simp
+
+@[simps!]
+def idxEquiv : l ≃ Fin l.length where
+  toFun := idx
+  invFun := mk
+  left_inv := by grind
+  right_inv := by grind
+
+/-- There is a coercion from `l : Type` to `α`. -/
+instance instCoeOutFinLength : CoeOut l α := ⟨val⟩
+
+@[simp] theorem mem_val {x : l} : x.val ∈ l := mem_of_getElem getElem_idx
 
 @[simp]
-protected theorem forall_coe (p : l → Prop) :
-    (∀ x : l, p x) ↔ ∀ (i : Nat) (hi : i < l.length), p ⟨i, hi⟩ := Fin.forall_iff
+protected theorem forall_mk (p : l → Prop) :
+    (∀ x : l, p x) ↔ ∀ (i : Fin l.length), p (mk i) :=
+  ⟨by grind, fun h x => x.mk_idx ▸ h x.idx⟩
 
 @[simp]
-protected theorem exists_coe (p : l → Prop) :
-    (∃ x : l, p x) ↔ ∃ (i : Nat) (hi : i < l.length), p ⟨i, hi⟩ := Fin.exists_iff
+protected theorem exists_mk (p : l → Prop) :
+    (∃ x : l, p x) ↔ ∃ (i : Fin l.length), p (mk i) :=
+  ⟨fun ⟨x, h⟩ => ⟨x.idx, x.mk_idx ▸ h⟩, by grind⟩
 
-instance : FinEnum l where
-  card := l.length
-  equiv := Equiv.refl _
+instance : DecidableEq l := idxEquiv.decidableEq
+
+instance : FinEnum l := {card := l.length, equiv := idxEquiv}
 
 theorem card_eq : Fintype.card l = l.length := (FinEnum.card_eq_fintypeCard).symm
 
-instance : IsEmpty ([] : List α) := Fin.isEmpty
+instance : IsEmpty ([] : List α) := idxEquiv.isEmpty
 
-instance {v : α} : Unique [v] := Fin.instUnique
+instance {v : α} : Unique [v] := idxEquiv.unique
 
-instance {v : α} : Inhabited (v :: l) := ⟨0⟩
+instance {v : α} : Inhabited (v :: l) := ⟨mk 0⟩
 
 @[simp, grind =]
 theorem coe_default {v : α} : (default : v :: l) = v := rfl
 
-@[simp, grind =]
-theorem coe_zero {v : α} : (0 : v :: l) = v := rfl
-
 /-- If `l = l'` then there's an equivalence between the appropriate types. -/
-@[simps]
-def cast {l' : List α} (h : l = l') : l ≃ l' where
-  toFun x := ⟨x.1, Nat.lt_of_lt_of_eq x.2 <| h ▸ rfl⟩
-  invFun x := ⟨x.1, Nat.lt_of_lt_of_eq x.2 <| h ▸ rfl⟩
+def cast (h : l = l') : l ≃ l' :=
+  idxEquiv.trans <| (finCongr <| h ▸ rfl).trans idxEquiv.symm
 
-/-- `x :: l` is equivalent to `Option l`. -/
-def consEquiv (v : α) : v :: l ≃ Option l where
-  toFun x := x.cases none some
-  invFun x := x.recOn 0 Fin.succ
-  left_inv x := x.cases rfl (fun _ => rfl)
-  right_inv x := x.recOn rfl (fun _ => rfl)
+theorem val_idx_cast (h : l = l') (x : l) : (cast h x).idx.val = x.idx.val := rfl
 
-@[simp, grind =]
-lemma consEquiv_zero {v : α} : l.consEquiv v 0 = none := rfl
+theorem val_cast (h : l = l') (x : l) : (cast h x).val = l'[x.idx]'(h ▸ by simp) := by
+  simp [cast, idxEquiv]
+
+def mapEquiv {α β} {l : List α} (f : α → β) : l ≃ l.map f :=
+  idxEquiv.trans <| (finCongr (length_map _).symm).trans idxEquiv.symm
 
 @[simp, grind =]
-lemma consEquiv_succ {v : α} {x : l} : l.consEquiv v x.succ = some x := rfl
+theorem val_idx_mapEquiv_apply {α β} {l : List α} {x : l} (f : α → β) :
+    (mapEquiv f x).idx.val = x.idx.val := rfl
 
-@[simp, grind =]
-lemma consEquiv_symm_none {v : α} : (l.consEquiv v).symm none = 0 := rfl
-
-@[simp, grind =]
-lemma consEquiv_symm_some {v : α} {x : l} : (l.consEquiv v).symm (some x) = x.succ := rfl
-
-def mapEquiv {α β} {l : List α} (f : α → β) : l ≃ l.map f where
-  toFun x := ⟨x.1, Nat.lt_of_lt_of_eq x.2 (l.length_map _).symm⟩
-  invFun x := ⟨x.1, Nat.lt_of_lt_of_eq x.2 (l.length_map _)⟩
-
-theorem mapEquiv_apply {α β} {l : List α} {x : l} (f : α → β) :
-    l.mapEquiv f x = f x := getElem_map _
+@[simp]
+theorem val_mapEquiv_apply {α β} {l : List α} {x : l} (f : α → β) :
+    (mapEquiv f x).val = f x.val := by grind
 
 theorem apply_mapEquiv_symm_apply {α β} {l : List α} (f : α → β) {x : l.map f} :
     f ((l.mapEquiv f).symm x) = x := (getElem_map _).symm
+
+def equivSigmaCount [BEq α] [LawfulBEq α] {l : List α} : l ≃ (x : α) × Fin (l.count x) where
+  toFun := l.idxToSigmaCount
+  invFun := l.sigmaCountToIdx
+  left_inv := l.leftInverse_sigmaCountToIdx_idxToSigmaCount
+  right_inv := l.rightInverse_sigmaCountToIdx_idxToSigmaCount
+
+instance [BEq α] [LawfulBEq α] {l : List α} : FinEnum ((x : α) × Fin (l.count x)) :=
+  FinEnum.ofEquiv _ equivSigmaCount.symm
+
+def Subperm.embedding [BEq α] [LawfulBEq α] {l₁ l₂ : List α} (h : l₁ <+~ l₂) : l₁ ↪ l₂ where
+  toFun := h.idxInj
+  inj' := h.idxInj_injective
+
+theorem Subperm.coe_embedding [BEq α] [LawfulBEq α] {l₁ l₂ : List α} (h : l₁ <+~ l₂) (x : l₁) :
+  (h.embedding x : α) = (x : α) := by simp [Subperm.embedding]
+
+def Perm.equiv [BEq α] [LawfulBEq α] {l₁ l₂ : List α} (h : l₁ ~ l₂) : l₁ ≃ l₂ where
+  toFun := h.idxBij
+  invFun := h.symm.idxBij
+  left_inv := h.idxBij_symm_leftInverse_idxBij
+  right_inv := h.idxBij_symm_rightInverse_idxBij
+
+theorem Perm.coe_equiv [BEq α] [LawfulBEq α] {l₁ l₂ : List α} (h : l₁ ~ l₂) (x : l₁) :
+  (h.equiv x : α) = (x : α) := by simp [Subperm.embedding]
+
+
+def permOfEquiv {l₁ l₂ : List α} (e : l₁ ≃ l₂)
+
+end ToType
+
 
 end List
